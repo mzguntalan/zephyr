15af3243de9f7d892c5ecd49e914c53bdfa8da5b
mzguntalan <mzguntalan@gmail.com>
2024-10-17 14:07:22 +0900

    Added a shape and activation with known gain types

--- zephyr/project_typing.py
+++ zephyr/project_typing.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from typing import NewType
 
 from jaxtyping import Array
@@ -5,3 +6,12 @@ from jaxtyping import Array
 # KeyArray = NewType("KeyArray", Array)
 
 KeyArray = Array
+Shape = tuple[int, ...]
+ActivationFunctionsWithKnownGain = Literal[
+    "linear",
+    "conv",  # corresponds to any conv (1d,2d, 1d transpose, etc)
+    "sigmoid",
+    "tanh",
+    "relu",
+    "leaky_relu",
+]
