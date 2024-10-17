1154f2ee193941c52d19d5b7b47faeab61c6fb97
mzguntalan <mzguntalan@gmail.com>
2024-10-17 13:23:31 +0900

    Added a normal distribution initializer

--- zephyr/building/initializers.py
+++ zephyr/building/initializers.py
@@ -5,6 +5,8 @@ from jax import numpy as jnp
 from jax import random
 from jaxtyping import Array
 
+from zephyr.project_typing import KeyArray
+
 Initializer = Callable[[random.PRNGKey, Tuple], Array]
 
 
@@ -18,3 +20,7 @@ def ones(key, shape):
 
 def uniform(key, shape):
     return random.uniform(key, shape)
+
+
+def normal(key: KeyArray, shape: tuple[int, ...]) -> Array:
+    return random.normal(key, shape)
