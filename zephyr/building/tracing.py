7b73feb588e9bababf4681be3f127d61ed2f33fd
mzguntalan <mzguntalan@gmail.com>
2024-10-20 12:02:10 +0900

    Tracer instead of Skeleton term
    Tracer seems more intuitive to more people
    
    I'll continue using Skeleton as it is more intuitive to me for the
    things it does right now.

--- zephyr/building/tracing.py
+++ zephyr/building/tracing.py
@@ -5,13 +5,12 @@ from typing import Union
 from jaxtyping import Array
 from jaxtyping import PyTree
 
-from zephyr.building.skeleton import Skeleton
 from zephyr.building.skeleton import Skeleton as Tracer
 from zephyr.project_typing import KeyArray
 
 
 def trace(f: Callable, key: KeyArray, *args) -> PyTree:
-    params = Skeleton(key)
+    params = Tracer(key)
     f(params, *args)
 
     return params.materialize()
