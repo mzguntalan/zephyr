f153b660b4bc3a01d006bb9ab4e4177fdd3d9005
mzguntalan <mzguntalan@gmail.com>
2024-11-04 14:32:04 +0900

    Corrected dropout scaling

--- zephyr/_nets/dropout.py
+++ zephyr/_nets/dropout.py
@@ -7,8 +7,9 @@ from zephyr.project_typing import KeyArray
 
 def dropout(key: KeyArray, x: Array, drop_probability: float) -> Array:
     mask = 1.0 * (random.uniform(key, x.shape) > drop_probability)
+    keep_probability = 1 - dropout
     scale_factor_so_that_the_sums_in_the_next_layers_are_in_the_same_range = 1 / (
-        drop_probability + 1e-19
+        keep_probability + 1e-19
     )
     return (
         apply_mask(x, mask)
