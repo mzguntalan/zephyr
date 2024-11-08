ac1a6ae2625217ab1f3f85bc1c3801db4a9124ec
mzguntalan <mzguntalan@gmail.com>
2024-11-08 23:36:02 +0900

    updated functions to get tags

--- zephyr/tags.py
+++ zephyr/tags.py
@@ -10,23 +10,14 @@ Tag = Any
 
 
 @flexible
-def get_last_n_tags(
-    p: dict[Tag, Leaf], n: int, context: list[Tag] = []
-) -> dict[Tag, list[Tag]]:
-    if not isinstance(p, dict):  # it is a leaf
-        return context[-n - 1 : -1]
-    return {k: get_last_n_tags(v, n, context=context + [k]) for k, v in p.items()}
+def get_immediate_tags(params: dict, context=[]):
+    if isinstance(params, Array):
+        return context
+    return {k: get_immediate_tags(v, context=[k]) for k, v in params.items()}
 
 
 @flexible
-def get_immediate_tags(
-    p: dict[Tag, Leaf], context: list[Tag] = []
-) -> dict[Tag, list[Tag]]:
-    return get_last_n_tags(p, 1)
-
-
-@flexible
-def get_lineage_tags(
-    p: dict[Tag, Leaf], context: list[Tag] = []
-) -> dict[Tag, list[Tag]]:
-    return get_last_n_tags(p, -1)
+def get_lineage_tags(params: dict, context=[]):
+    if isinstance(params, Array):
+        return context
+    return {k: get_lineage_tags(v, context=context + [k]) for k, v in params.items()}
